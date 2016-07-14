a4b6432cfad2f40b2af24ebfbe2bf60cb66e7a4a
jinwonchoi <jinnonsbox@gmail.com>
2016-07-15 01:48:00 +0900

    invalidate를 목적으로 추가

---
+++ RingCatcher/LibRingMsgEditor/src/main/java/com/gencode/ringmsgeditor/OnImageSizeChangeListener.java
@@ -0,0 +1,10 @@
+package com.gencode.ringmsgeditor;
+
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016-07-15.
+ */
+public interface OnImageSizeChangeListener {
+    void OnImageSizeChanged(View v);
+}
